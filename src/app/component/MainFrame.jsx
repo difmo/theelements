5fa29cd75c5e039d7153d1c41eafcb84536fec28
vimal raj <vimalraj87464@gmail.com>
2024-12-04 03:44:08 -0800

    commit

--- src/app/component/MainFrame.jsx
+++ src/app/component/MainFrame.jsx
@@ -24,7 +24,7 @@ const headerdata = [
 
 const MainFrame = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [fadeIn, setFadeIn] = useState(false);
+  const [fadeIn, setFadeIn] = useState(false); 
   const [fadeInText, setFadeInText] = useState(false);
 
   useEffect(() => {
@@ -76,40 +76,20 @@ const MainFrame = () => {
         />
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 bg-gradient-to-t from-blue-800 via-transparent to-transparent opacity-60"></div>
-        {/* <div className="absolute flex flex-col items-start justify-center w-full h-full px-4 my-5 sm:px-16 md:px-16">
-          <div
-            className={`w-full text-[24px] sm:text-[36px] md:text-[48px] lg:text-[60px] leading-[36px] sm:leading-[45px] md:leading-[55px] lg:leading-[66.48px] text-[#F3F4F6] font-quattrocento  capitalize transition-opacity duration-1000 ease-in-out ${
-              fadeInText ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            {headerdata[currentIndex].heading}
-          </div>
-          <div
-            className={`w-full text-[16px] sm:text-[20px] md:text-[28px] lg:text-[36px] leading-[24px] sm:leading-[28px] md:leading-[36px] lg:leading-[50px] text-[#F3F4F6] font-medium capitalize transition-opacity duration-1000 ease-in-out ${
-              fadeInText ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            {headerdata[currentIndex].description}
-          </div>
-          <div className="mt-6 sm:mt-8">
-            <button className="text-[#F3F4F6] text-lg font-medium bg-[#1A95CB] rounded-[30px] px-8 py-3">
-              Contact Us
-            </button>
-          </div>
-        </div> */}
+      
 
-<div className="absolute flex flex-col items-start justify-center w-full h-full px-4 my-5 sm:px-16 md:px-16">
+<div className="absolute flex flex-col items-start justify-center w-full h-full px-10 my-5 sm:px-16 md:px-16">
   <div
     className={`w-full text-[24px] sm:text-[36px] md:text-[48px] lg:text-[60px] leading-[36px] sm:leading-[45px] md:leading-[55px] lg:leading-[66.48px] text-[#F3F4F6] font-quattrocento capitalize transition-opacity duration-1000 ease-in-out ${
       fadeInText ? "opacity-100" : "opacity-0"
-    } text-center sm:text-left`}
+    } text-center sm:text-center`}
   >
     {headerdata[currentIndex].heading}
   </div>
   <div
     className={`w-full text-[16px] sm:text-[20px] md:text-[28px] lg:text-[36px] leading-[24px] sm:leading-[28px] md:leading-[36px] lg:leading-[50px] text-[#F3F4F6] font-medium capitalize transition-opacity duration-1000 ease-in-out ${
       fadeInText ? "opacity-100" : "opacity-0"
-    } text-center sm:text-left`}
+    } text-center sm:text-center`}
   >
     {headerdata[currentIndex].description}
   </div>
