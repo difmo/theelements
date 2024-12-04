261e54033bd6d603f8e3952c941a277cef8d4c0c
codeservier <codeservir@gmail.com>
2024-12-04 17:32:13 +0530

    save

--- src/app/component/Whoweserve.jsx
+++ src/app/component/Whoweserve.jsx
@@ -30,7 +30,7 @@ const Whoweserve = () => {
       ]
     },
     {
-      title: 'Patients',
+      title: 'Help Tech',
       image: weserve3,
       listItems: [
         'Enhance insights with AI in existing solutions.',
