d1c598dba80db54e54eab6476bf0283eada26a81
difmo <difmotech@gmail.com>
2024-12-11 13:48:17 +0530

    commit

--- src/app/component/OurServices.jsx
+++ src/app/component/OurServices.jsx
@@ -229,7 +229,7 @@ const OurServices2 = () => {
               <div
                 key={tabIndex}
                 className={`flex items-center mb-2  gap-2  px-10 md:w-[380px] md:h-[124px] space-x-4 bg-white rounded-full cursor-pointer ${
-                  activeTab === tabIndex ? "bg-[#dbeafe]" : "bg-white"
+                  activeTab === tabIndex ? "bg-[#d2e5fd]" : "bg-white"
                 }`}
                 onClick={() => handleTabClick(tabIndex)}
               >
