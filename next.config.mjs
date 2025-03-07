64f060d954ee1188585456dbf34c5aaf1c0c2ffd
dineshappdeveloper <dineshkumartlbupi@gmail.com>
2025-03-07 12:11:24 +0530

    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ["firebasestorage.googleapis.com"],
   },
-  distDir: 'dist',  
-  reactStrictMode: true, 
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false, 
-      };
-    }
-    return config;
-  },
-  env: {
-    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-    SANITY_DATASET: process.env.SANITY_DATASET,
-  },
-  i18n: {
-    locales: ['en', 'fr', 'de'],  
-    defaultLocale: 'en',  
-  },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+export default nextConfig; 
