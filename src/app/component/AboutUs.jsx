f95ab67f7dd5b0445f21aa2117972d1baaacf0cd
dineshappdeveloper <dineshkumartlbupi@gmail.com>
2025-04-04 18:35:02 +0530

    save

--- src/app/component/AboutUs.jsx
+++ src/app/component/AboutUs.jsx
@@ -3,6 +3,7 @@ import Heading from "./Heading";
 import img from '../assets/weserve3.jpg'
 import Image from "next/image";
 import aboutusimg from "../assets/images/aboutus.svg";
+import aboutus from "../assets/images/aboutusimg.jpeg";
 
 const AboutUs = () => {
   return (
@@ -16,29 +17,32 @@ const AboutUs = () => {
       <div className="max-w-6xl mx-auto px-8 flex flex-col md:flex-row items-center gap-8">
         <div className="md:w-1/2 relative">
           <Image
-            src={aboutusimg}a
+            src={aboutus}
             alt="Business Meeting"
             className="  w-[500px]  "
           />
         </div>
-        <div className=" border-l w-2 hidden md:flex rounded-full bg-primary-900/40  h-[500px]"></div>
+        {/* <div className=" border-l w-2 hidden md:flex rounded-full bg-primary-900/40  h-[500px]"></div> */}
         <div className="md:w-1/2">
-          <h2 className="text-xl font-bold text-primary-900 underline font-quattrocento text-secborder-secondary-600 mb-2 ">  Who We Are  </h2>
-          <h1 className="text-xl text-primary-900 font-bold mb-4 font-mulish">
-            8th Element is a technology company specializing in healthcare solutions. We help businesses use technology to improve operations, enhance patient care, and stay ahead in a changing industry.
+          <h2 className="text-xl font-bold text-primary-900 font-quattrocento text-secborder-secondary-600 mb-2 ">About 8thElement</h2>
+          <h1 className="text-xl text-primary-900/80 font-bold mb-4 font-mulish">
+          At 8th Element, we harness technology to transform healthcare. Our mission is to help organizations streamline operations, improve patient care, and make informed decisions through innovation. With deep industry expertise, we deliver practical, forward-thinking solutions that drive efficiency and support growth. 
 
 
           </h1>
           <p className="text-gray-600 mb-6  font-mulish">
-          At 8thE, we believe technology can transform healthcare. With our expertise, we provide smart, effective solutions that boost efficiency, improve decision-making, and support better health outcomes. 
+          We believe technology has the power to revolutionize healthcare, and we are dedicated to bridging the gap between the two—empowering our clients to innovate, scale, and lead in a rapidly evolving world.
 
 
+          </p>
+          <p className="text-gray-600 mb-6  font-mulish">
+          Thank you for being part of this journey. Together, we can shape the future of healthcare.
           </p>
           {/* <button className= "bg-secondary-600 border-secondary-600 text-white px-6 py-2 font-mulish rounded-lg font-bold hover:bg-orange-600 transition">
             Read More
           </button> */}
           
-          <div className="flex justify-start space-x-8 mt-8">   
+          {/* <div className="flex justify-start space-x-8 mt-8">   
             <div className="border border-gray-300 rounded-xl p-3 ">
               <h2 className="  text-3xl font-bold text-gray-600 font-mulish">500+</h2>
               <p className="text-gray-600 font-mulish">Satisfied Customers</p>
@@ -51,7 +55,7 @@ const AboutUs = () => {
               <h2 className="text-3xl font-bold text-gray-700 font-mulish">40+</h2>
               <p className="text-gray-600 font-mulish">Projects Completed</p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
